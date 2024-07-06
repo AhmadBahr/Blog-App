40a68f6faa712175b3cf5b63c4ab4520fb25a059
Ahmad Bahr <ahmad.bahr@mubs.edu.lb>
2024-07-07 01:33:07 +0300

    setting up outlet

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,45 @@
+import { Outlet } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Route, Outlet } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Write from "./pages/Write";
+import Single from "./pages/Single";
+import Navbar from "./components/Navbar";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>This is home!</div>,
+  },
+  {
+    path: "/register",
+    element: <Register/>,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+  {
+    path: "/Write",
+    element: <Write/>,
+  },
+  {
+    path: "/Single",
+    element: <Single/>,
+  },
+]);
+
 function App() {
-  return <div>Hello World!</div>;
+  return <RouterProvider router={router} />;
 }
+
+const Layout = () => {
+  return (
+    <>
+     <Navbar/>
+    <Outlet/>
+    <Footer/>
+    </>
+  );
+};
 
 export default App;
