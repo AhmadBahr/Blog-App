c1f09f9203a5d9841e7c3985a809b613dde9965f
Ahmad Bahr <ahmad.bahr@mubs.edu.lb>
2024-07-08 12:32:32 +0300

    routing update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,45 +1,65 @@
-import { Outlet } from 'react-router-dom';
-import { createBrowserRouter, RouterProvider, Route, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Write from "./pages/Write";
 import Single from "./pages/Single";
 import Navbar from "./components/Navbar";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>This is home!</div>,
-  },
-  {
-    path: "/register",
-    element: <Register/>,
-  },
-  {
-    path: "/login",
-    element: <Login/>,
-  },
-  {
-    path: "/Write",
-    element: <Write/>,
-  },
-  {
-    path: "/Single",
-    element: <Single/>,
-  },
-]);
-
-function App() {
-  return <RouterProvider router={router} />;
-}
 
 const Layout = () => {
   return (
     <>
-     <Navbar/>
-    <Outlet/>
-    <Footer/>
+      <Navbar />
+      <Outlet />
+      <Footer />
     </>
   );
 };
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children:[
+      {
+      path:"/",
+      element:<Home/>,
+      },
+      {
+        path:"/post/:id",
+        element:<Single/>,
+      },
+      {
+        path:"/write",
+        element:<Write/>,
+      },
+    ]
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/Write",
+    element: <Write />,
+  },
+  {
+    path: "/Single",
+    element: <Single />,
+  },
+]);
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+      <RouterProvider router={router} />
+      </div>
+    </div>
+  );
+}
 
 export default App;
