907c0e0381d3a39325bf822a35b4f6ce83c4685e
Ahmad Bahr <ahmad.bahr@mubs.edu.lb>
2024-07-08 23:37:31 +0300

    reigster page done

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className='auth'>
+        <h1>Register</h1>
+        <form>
+            <input required type="text" placeholder='username' />
+            <input required type="email" placeholder='email' />
+            <input required type="password" placeholder='password' />
+            <button>Register</button>
+        </form>
+        <span>Do you have an account? <a href="/login">Login</a></span>
+    </div>
   )
 }
 
