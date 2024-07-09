ebaab278fb848a0ae1128b0efd5fcc8de1a5df81
Ahmad Bahr <ahmad.bahr@mubs.edu.lb>
2024-07-09 22:16:49 +0300

    adding dummy data

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,72 @@
 import React from 'react'
 
 const Home = () => {
+  const posts = [
+    {
+        id: 1,
+        title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+        description: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+        image: "https://images.pexels.com/photos/1001/pexels-photo-1001.jpeg"
+      },
+      {
+        id: 2,
+        title: "qui est esse",
+        description: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
+        image: "https://images.pexels.com/photos/1002/pexels-photo-1002.jpeg"
+      },
+      {
+        id: 3,
+        title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+        description: "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut",
+        image: "https://images.pexels.com/photos/1003/pexels-photo-1003.jpeg"
+      },
+      {
+        id: 4,
+        title: "eum et est occaecati",
+        description: "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit",
+        image: "https://images.pexels.com/photos/1004/pexels-photo-1004.jpeg"
+      },
+      {
+        id: 5,
+        title: "nesciunt quas odio",
+        desc: "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque",
+        img: "https://images.pexels.com/photos/1005/pexels-photo-1005.jpeg"
+      },
+      {
+        "id": 6,
+        "title": "dolorem eum magni eos aperiam quia",
+        desc: "ut aspernatur corporis harum nihil quis provident sequi\nmollitia nobis aliquid molestiae\nperspiciatis et ea nemo ab reprehenderit accusantium quas\nvoluptate dolores velit et doloremque molestiae",
+        img: "https://images.pexels.com/photos/1006/pexels-photo-1006.jpeg"
+      },
+      {
+        "id": 7,
+        "title": "magnam facilis autem",
+        desc: "dolore placeat quibusdam ea quo vitae\nmagni quis enim qui quis quo nemo aut saepe\nquidem repellat excepturi ut quia\nsunt ut sequi eos ea sed quas",
+        img: "https://images.pexels.com/photos/1007/pexels-photo-1007.jpeg"
+      },
+      {
+        "id": 8,
+        "title": "dolorem dolore est ipsam",
+        desc: "dignissimos aperiam dolorem qui eum\nfacilis quibusdam animi sint suscipit qui sint possimus cum\nquaerat magni maiores excepturi\nipsam ut commodi dolor voluptatum modi aut vitae",
+        img: "https://images.pexels.com/photos/1008/pexels-photo-1008.jpeg"
+      },
+      {
+        "id": 9,
+        "title": "nesciunt iure omnis dolorem tempora et accusantium",
+        desc: "consectetur animi nesciunt iure dolore\nenim quia ad\nveniam autem ut quam aut nobis\net est aut quod aut provident voluptas autem voluptas",
+        img: "https://images.pexels.com/photos/1009/pexels-photo-1009.jpeg"
+      },
+      {
+        "id": 10,
+        "title": "optio molestias id quia eum",
+        desc: "quo et expedita modi cum officia vel magni\ndoloribus qui repudiandae\nvero nisi sit\nquos veniam quod sed accusamus veritatis error",
+        img: "https://images.pexels.com/photos/1010/pexels-photo-1010.jpeg"
+      }
+    ]    
   return (
-    <div>Home</div>
+    <div className='home'>
+
+    </div>
   )
 }
 
