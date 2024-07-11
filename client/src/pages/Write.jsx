29db50092cfd4726ed59462ce2421140c526eb2e
Ahmad Bahr <ahmad.bahr@mubs.edu.lb>
2024-07-11 22:44:51 +0300

    adding react quill

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const Write = () => {
   return (
-    <div>Write</div>
+    <div className='add'>
+      <div className='content'>
+        <input type="text" placeholder='Title' />
+        <textarea placeholder='Tell your story...'></textarea>
+      </div>
+      <div className='publish'>
+        Publish
+      </div>
+      <div className='publish'>
+        Preview
+      </div>
+      <div className='menu'>
+        <div className='item1'>i1</div>
+        <div className='item2'>i2</div>
+      </div>
+    </div>
   )
 }
 
