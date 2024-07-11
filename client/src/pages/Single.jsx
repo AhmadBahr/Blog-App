888fc8bfa3df7a9121fa0ef031d636ecd3f8a273
Ahmad Bahr <ahmad.bahr@mubs.edu.lb>
2024-07-11 20:14:08 +0300

    adding the edit and delete icons

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import Edit from '../img/edit.png'
+import Delete from '../img/delete.png'
 
 const Single = () => {
   return (
-    <div>Single</div>
-  )
-}
+    <div className="single">
+      <div className="content">
+        <img src="https://randomwordgenerator.com/img/picture-generator/54e4d7434256a414f1dc8460962e33791c3ad6e04e5074417c2f7dd59748c3_640.jpg"></img>
+        <div className="user">
+          <img src="https://randomwordgenerator.com/img/picture-generator/54e4d7434256a414f1dc8460962e33791c3ad6e04e5074417c2f7dd59748c3_640.jpg"></img>
+          <div className="info">
+            <span>John Doe</span>
+            <p>Posted 2 days ago</p>
+          </div>
+          <div className="edit">
+            <img src={Edit} alt = ""/>
+            <img src={Delete} alt=""/>
+          </div>
+        </div>
+      </div>
+      <div className="menu">m</div>
+    </div>
+  );
+};
 
-export default Single
+export default Single;
