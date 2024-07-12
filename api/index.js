698247b09b5f4b8e278671372d2ba7dba0308fee
Ahmad Bahr <ahmad.bahr@mubs.edu.lb>
2024-07-12 17:13:09 +0300

    setting folder structure

--- api/index.js
+++ api/index.js
@@ -1,12 +1,10 @@
 import express from "express";
+import postRoutes from "./routes/posts.js";
 
 const app = express();
 
 app.use(express.json());
-
-app.get("/test", (req, res) => {
-    res.send("Hello World!");
-});
+app.use("/api/posts", postRoutes);
 
 app.listen(8800, () => {
     console.log("Conntectd!");
