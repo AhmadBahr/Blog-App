fd19e4839052d9c7c2a4438b6120b056a243df4c
Ahmad Bahr <ahmad.bahr@mubs.edu.lb>
2024-07-13 23:29:02 +0300

    handleChange

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,16 +1,33 @@
 import React from 'react'
 
 const Register = () => {
+  const [inputs, setInputs] = React.useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  }
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+
+  }
+
+
+
   return (
     <div className='auth'>
-        <h1>Register</h1>
-        <form>
-            <input required type="text" placeholder='username' />
-            <input required type="email" placeholder='email' />
-            <input required type="password" placeholder='password' />
-            <button>Register</button>
-        </form>
-        <span>Do you have an account? <a href="/login">Login</a></span>
+      <h1>Register</h1>
+      <form>
+        <input required type="text" placeholder='username' onChange={handleChange} name='username' />
+        <input required type="email" placeholder='email' onChange={handleChange} name='email' />
+        <input required type="password" placeholder='password' onChange={handleChange} name='password' />
+        <button onClick={handleSubmit}>Register</button>
+      </form>
+      <span>Do you have an account? <a href="/login">Login</a></span>
     </div>
   )
 }
