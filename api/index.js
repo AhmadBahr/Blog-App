07019044bbf94b52f3b71d143f67c21b03ae8870
Ahmad Bahr <ahmad.bahr@mubs.edu.lb>
2024-07-15 17:05:55 +0300

    adding multer middleware

--- api/index.js
+++ api/index.js
@@ -3,11 +3,23 @@ import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import cookieParser from "cookie-parser";
+import multer from "multer";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
+
+const upload = multer({ dest: './uploads/' }); 
+
+app.post('/api/upload', upload.single('file'), (req, res) => {
+  const file = req.file;
+  res.status(200).json(file.filename);
+})
+
+
+
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
