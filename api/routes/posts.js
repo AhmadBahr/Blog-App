0b82aaf28d2fb5fd56184321a28b9a1f3f1277bf
Ahmad Bahr <ahmad.bahr@mubs.edu.lb>
2024-07-15 11:38:02 +0300

    postcontroller: getallPosts

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,5 +1,11 @@
 import express from "express"
+import { getPosts } from "../controllers/posts.js"
 
 const router = express.Router();
 
+router.get("/", getPosts)
+router.post("/:id", getPost)
+router.post("/", addPost)
+router.update("/", updatePost)
+router.delete("/", deletePost)
 export default router
